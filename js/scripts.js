b2e31d03a93efa1edab66a7149143337e58d92aa
jeroseUX <jacksonerose94@gmail.com>
2022-07-21 16:40:58 -0600

    Toggle Menu
    Added a bunch of code for toggle menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
