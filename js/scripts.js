f72babf0705fb27f09c221a83426eba0c8e78ff0
jeroseUX <jacksonerose94@gmail.com>
2022-07-25 14:48:55 -0600

    Requested Revisions
    I removed the contact links and jQuery from my scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,3 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="js/scripts.js"></script>
